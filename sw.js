cbb3a741390a515158037446dcdace177c1212d6
atuki1201 <atuki1201@icloud.com>
2026-09-25 18:03:01 +0900

    オフライン対応を追加

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE="kijo-v6";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./checks.js","./app.js","./manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
