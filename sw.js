24e92a7d4dcbf60a3c93710eb1ea8ba8dd4045fa
atuki1201 <atuki1201@icloud.com>
2026-09-25 18:20:54 +0900

    キャッシュを更新

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const CACHE="kijo-v7";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./checks.js","./app.js","./manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="kijo-v8";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./checks.js","./app.js","./manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
